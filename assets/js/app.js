843c16082287fa13bc535ba3025bbcc2118d8c6a
Md Lihan <freelancerlihansocial@gmail.com>
2022-12-15 22:50:19 +0600

    change

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,11 +2,11 @@ let bars = document.querySelector(".bars i");
 let menu = document.querySelector(".menu");
 let faXmark = document.querySelector(".fa-xmark");
 bars.addEventListener("click", (e) => {
-  if (e) {
-    menu.classList.toggle("active");
-    bars.classList.remove("fa-bars");
-    bars.classList.add("fa-xmark");
-  } else {
-    faXmark.classList.add("fa-bars");
+  menu.classList.toggle("active");
+  bars.classList.remove("fa-bars");
+  bars.classList.add("fa-xmark");
+  if (!menu.classList.contains("active")) {
+    bars.classList.remove("fa-xmark");
+    bars.classList.add("fa-bars");
   }
 });
