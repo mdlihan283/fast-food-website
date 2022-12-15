eee4a49ad2fc2dfca27c3a632014981a1892d4f5
Md Lihan <freelancerlihansocial@gmail.com>
2022-12-15 23:02:33 +0600

    changed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,6 @@
 let bars = document.querySelector(".bars i");
 let menu = document.querySelector(".menu");
+let menuLi = document.querySelectorAll(".menu li");
 let faXmark = document.querySelector(".fa-xmark");
 bars.addEventListener("click", (e) => {
   menu.classList.toggle("active");
@@ -9,4 +10,12 @@ bars.addEventListener("click", (e) => {
     bars.classList.remove("fa-xmark");
     bars.classList.add("fa-bars");
   }
+});
+
+menuLi.forEach((element) => {
+  element.addEventListener("click", () => {
+    menu.classList.remove("active");
+    bars.classList.remove("fa-xmark");
+    bars.classList.add("fa-bars");
+  });
 });
